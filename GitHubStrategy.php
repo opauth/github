e75cadff3d433e62332d23472d816d26c802da4f
U-Zyn Chua <chua@uzyn.com>
2012-07-23 14:13:08 +0800

    GitHub strategy. Done

--- GitHubStrategy.php
+++ GitHubStrategy.php
@@ -1,23 +1,21 @@
 <?php
 /**
- * Google strategy for Opauth
- * based on https://developers.google.com/accounts/docs/OAuth2
+ * GitHub strategy for Opauth
  * 
  * More information on Opauth: http://opauth.org
  * 
  * @copyright    Copyright © 2012 U-Zyn Chua (http://uzyn.com)
  * @link         http://opauth.org
- * @package      Opauth.GoogleStrategy
+ * @package      Opauth.GitHubStrategy
  * @license      MIT License
  */
 
 /**
- * Google strategy for Opauth
- * based on https://developers.google.com/accounts/docs/OAuth2
+ * GitHub strategy for Opauth
  * 
- * @package			Opauth.Google
+ * @package			Opauth.GitHub
  */
-class GoogleStrategy extends OpauthStrategy{
+class GitHubStrategy extends OpauthStrategy {
 	
 	/**
 	 * Compulsory config keys, listed as unassociative arrays
@@ -27,30 +25,27 @@ class GoogleStrategy extends OpauthStrategy{
 	/**
 	 * Optional config keys, without predefining any default values.
 	 */
-	public $optionals = array('redirect_uri', 'scope', 'state', 'access_type', 'approval_prompt');
+	public $optionals = array('redirect_uri', 'scope', 'state');
 	
 	/**
 	 * Optional config keys with respective default values, listed as associative arrays
 	 * eg. array('scope' => 'email');
 	 */
 	public $defaults = array(
-		'redirect_uri' => '{complete_url_to_strategy}oauth2callback',
-		'scope' => 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+		'redirect_uri' => '{complete_url_to_strategy}oauth2callback'
 	);
 	
 	/**
 	 * Auth request
 	 */
-	public function request(){
-		$url = 'https://accounts.google.com/o/oauth2/auth';
+	public function request() {
+		$url = 'https://github.com/login/oauth/authorize';
 		$params = array(
 			'client_id' => $this->strategy['client_id'],
-			'redirect_uri' => $this->strategy['redirect_uri'],
-			'response_type' => 'code',
-			'scope' => $this->strategy['scope']
+			'redirect_uri' => $this->strategy['redirect_uri']
 		);
 
-		foreach ($this->optionals as $key){
+		foreach ($this->optionals as $key) {
 			if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
 		}
 		
@@ -60,48 +55,48 @@ class GoogleStrategy extends OpauthStrategy{
 	/**
 	 * Internal callback, after OAuth
 	 */
-	public function oauth2callback(){
-		if (array_key_exists('code', $_GET) && !empty($_GET['code'])){
+	public function oauth2callback() {
+		if (array_key_exists('code', $_GET) && !empty($_GET['code'])) {
 			$code = $_GET['code'];
-			$url = 'https://accounts.google.com/o/oauth2/token';
+			$url = 'https://github.com/login/oauth/access_token';
+			
 			$params = array(
 				'code' => $code,
 				'client_id' => $this->strategy['client_id'],
 				'client_secret' => $this->strategy['client_secret'],
 				'redirect_uri' => $this->strategy['redirect_uri'],
-				'grant_type' => 'authorization_code'
 			);
+			if (!empty($this->strategy['state'])) $params['state'] = $this->strategy['state'];
+			
 			$response = $this->serverPost($url, $params, null, $headers);
+			parse_str($response, $results);
 			
-			$results = json_decode($response);
-			
-			if (!empty($results) && !empty($results->access_token)){
-				$userinfo = $this->userinfo($results->access_token);
+			if (!empty($results) && !empty($results['access_token'])) {
+				$user = $this->user($results['access_token']);
 				
 				$this->auth = array(
-					'provider' => 'Google',
-					'uid' => $userinfo->id,
-					'info' => array(
-						'name' => $userinfo->name,
-						'email' => $userinfo->email,
-						'first_name' => $userinfo->given_name,
-						'last_name' => $userinfo->family_name,
-						'image' => $userinfo->picture
-					),
+					'uid' => $user['id'],
+					'info' => array(),
 					'credentials' => array(
-						'token' => $results->access_token,
-						'expires' => date('c', time() + $results->expires_in)
+						'token' => $results['access_token']
 					),
-					'raw' => $userinfo
+					'raw' => $user
 				);
 				
-				if (!empty($userinfo->link)) $this->auth['info']['urls']['google'] = $userinfo->link;
+				$this->mapProfile($user, 'name', 'info.name');
+				$this->mapProfile($user, 'blog', 'info.urls.blog');
+				$this->mapProfile($user, 'avatar_url', 'info.image');
+				$this->mapProfile($user, 'bio', 'info.description');
+				$this->mapProfile($user, 'login', 'info.nickname');
+				$this->mapProfile($user, 'html_url', 'info.urls.github');
+				$this->mapProfile($user, 'email', 'info.email');
+				$this->mapProfile($user, 'location', 'info.location');
+				$this->mapProfile($user, 'url', 'info.urls.github_api');
 				
 				$this->callback();
 			}
-			else{
+			else {
 				$error = array(
-					'provider' => 'Google',
 					'code' => 'access_token_error',
 					'message' => 'Failed when attempting to obtain access token',
 					'raw' => array(
@@ -113,9 +108,8 @@ class GoogleStrategy extends OpauthStrategy{
 				$this->errorCallback($error);
 			}
 		}
-		else{
+		else {
 			$error = array(
-				'provider' => 'Google',
 				'code' => 'oauth2callback_error',
 				'raw' => $_GET
 			);
@@ -125,23 +119,23 @@ class GoogleStrategy extends OpauthStrategy{
 	}
 	
 	/**
-	 * Queries Google API for user info
+	 * Queries GitHub v3 API for user info
 	 *
 	 * @param string $access_token 
 	 * @return array Parsed JSON results
 	 */
-	private function userinfo($access_token){
-		$userinfo = $this->serverGet('https://www.googleapis.com/oauth2/v1/userinfo', array('access_token' => $access_token), null, $headers);
-		if (!empty($userinfo)){
-			return json_decode($userinfo);
+	private function user($access_token) {
+		$user = $this->serverGet('https://api.github.com/user', array('access_token' => $access_token), null, $headers);
+
+		if (!empty($user)) {
+			return $this->recursiveGetObjectVars(json_decode($user));
 		}
-		else{
+		else {
 			$error = array(
-				'provider' => 'Google',
 				'code' => 'userinfo_error',
-				'message' => 'Failed when attempting to query for user information',
+				'message' => 'Failed when attempting to query GitHub v3 API for user information',
 				'raw' => array(
-					'response' => $userinfo,
+					'response' => $user,
 					'headers' => $headers
 				)
 			);
