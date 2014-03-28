1256d4065cb8eb7f86a9c3335bc86875ba4782dd
Ceeram <c33ram@gmail.com>
2014-03-28 09:52:14 +0100

    tabs to spaces

--- src/GitHub.php
+++ src/GitHub.php
@@ -17,96 +17,100 @@ use Opauth\Opauth\AbstractStrategy;
 /**
  * GitHub strategy for Opauth
  *
- * @package			Opauth.GitHub
+ * @package            Opauth.GitHub
  */
-class GitHub extends AbstractStrategy {
+class GitHub extends AbstractStrategy
+{
 
-	/**
-	 * Compulsory config keys, listed as unassociative arrays
-	 */
-	public $expects = array('client_id', 'client_secret');
+    /**
+     * Compulsory config keys, listed as unassociative arrays
+     */
+    public $expects = array('client_id', 'client_secret');
 
-	/**
-	 * Optional config keys, without predefining any default values.
-	 */
-	public $optionals = array('redirect_uri', 'scope', 'state');
+    /**
+     * Optional config keys, without predefining any default values.
+     */
+    public $optionals = array('redirect_uri', 'scope', 'state');
 
-	public $responseMap = array(
-		'uid' => 'id',
-		'name' => 'login',
-		'info.name' => 'name',
-		'info.urls.blog' => 'blog',
-		'info.image' => 'avatar_url',
-		'info.description' => 'bio',
-		'info.nickname' => 'login',
-		'info.urls.github' => 'html_url',
-		'info.email' => 'email',
-		'info.location' => 'location',
-		'info.urls.github_api' => 'url',
-	);
+    public $responseMap = array(
+        'uid' => 'id',
+        'name' => 'login',
+        'info.name' => 'name',
+        'info.urls.blog' => 'blog',
+        'info.image' => 'avatar_url',
+        'info.description' => 'bio',
+        'info.nickname' => 'login',
+        'info.urls.github' => 'html_url',
+        'info.email' => 'email',
+        'info.location' => 'location',
+        'info.urls.github_api' => 'url',
+    );
 
-	/**
-	 * Auth request
-	 */
-	public function request() {
-		$url = 'https://github.com/login/oauth/authorize';
-		$params = array(
-			'client_id' => $this->strategy['client_id'],
-			'redirect_uri' => $this->callbackUrl()
-		);
-		$params = $this->addParams($this->optionals, $params);
+    /**
+     * Auth request
+     */
+    public function request()
+    {
+        $url = 'https://github.com/login/oauth/authorize';
+        $params = array(
+            'client_id' => $this->strategy['client_id'],
+            'redirect_uri' => $this->callbackUrl()
+        );
+        $params = $this->addParams($this->optionals, $params);
 
-		$this->http->redirect($url, $params);
-	}
+        $this->http->redirect($url, $params);
+    }
 
-	/**
-	 * Internal callback, after OAuth
-	 */
-	public function callback() {
-		if (empty($_GET['code'])) {
-			return $this->response($_GET, array('code' => 'callback_error'));
-		}
+    /**
+     * Internal callback, after OAuth
+     */
+    public function callback()
+    {
+        if (empty($_GET['code'])) {
+            return $this->response($_GET, array('code' => 'callback_error'));
+        }
 
-		$response = $this->accessToken($_GET['code']);
-		parse_str($response, $results);
-		if (empty($results['access_token'])) {
-			$error = array(
-				'code' => 'access_token_error',
-				'message' => 'Failed when attempting to obtain access token',
-			);
-			return $this->response($response, $error);
-		}
+        $response = $this->accessToken($_GET['code']);
+        parse_str($response, $results);
+        if (empty($results['access_token'])) {
+            $error = array(
+                'code' => 'access_token_error',
+                'message' => 'Failed when attempting to obtain access token',
+            );
+            return $this->response($response, $error);
+        }
 
-		$data = array('access_token' => $results['access_token']);
-		$user = $this->http->get('https://api.github.com/user', $data);
-		$user = $this->recursiveGetObjectVars(json_decode($user));
-		if (empty($user) || isset($user['message'])) {
-			$error = array(
-				'code' => 'userinfo_error',
-				'message' => 'Failed when attempting to query GitHub v3 API for user information'
-			);
-			if (isset($user['message'])) {
-				$error['message'] = $user['message'];
-			}
-			return $this->response($user, $error);
-		}
+        $data = array('access_token' => $results['access_token']);
+        $user = $this->http->get('https://api.github.com/user', $data);
+        $user = $this->recursiveGetObjectVars(json_decode($user));
+        if (empty($user) || isset($user['message'])) {
+            $error = array(
+                'code' => 'userinfo_error',
+                'message' => 'Failed when attempting to query GitHub v3 API for user information'
+            );
+            if (isset($user['message'])) {
+                $error['message'] = $user['message'];
+            }
+            return $this->response($user, $error);
+        }
 
-		$response = $this->response($user);
-		$response->credentials = array('token' => $results['access_token']);
-		return $response;
-	}
+        $response = $this->response($user);
+        $response->credentials = array('token' => $results['access_token']);
+        return $response;
+    }
 
-	protected function accessToken($code) {
-		$url = 'https://github.com/login/oauth/access_token';
-		$params = array(
-			'code' => $code,
-			'client_id' => $this->strategy['client_id'],
-			'client_secret' => $this->strategy['client_secret'],
-			'redirect_uri' => $this->callbackUrl(),
-		);
-		$params = $this->addParams($this->optionals, $params);
+    protected function accessToken($code)
+    {
+        $url = 'https://github.com/login/oauth/access_token';
+        $params = array(
+            'code' => $code,
+            'client_id' => $this->strategy['client_id'],
+            'client_secret' => $this->strategy['client_secret'],
+            'redirect_uri' => $this->callbackUrl(),
+        );
+        $params = $this->addParams($this->optionals, $params);
 
-		return $this->http->post($url, $params);
-	}
+        return $this->http->post($url, $params);
+    }
 
 }
